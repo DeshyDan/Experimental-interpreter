6463d7a892621133d9b8a74814f4241999970459
Deshy Dan <devdeshy@gmail.com>
2025-01-26 04:03:24 +0200

    Improve documentation

--- src/main/com/deshyan/mandela/Interpreter.java
+++ src/main/com/deshyan/mandela/Interpreter.java
@@ -4,34 +4,33 @@ import static com.deshyan.mandela.TokenType.*;
 
 public class Interpreter {
     private final char[] text;
-    private int textIndex;
+    private int tokenizerIndex;
     private Token currentToken;
 
     public Interpreter(String text) {
         this.text = text.toCharArray();
-        this.textIndex = 0;
+        this.tokenizerIndex = 0;
     }
 
     /**
-     * Responsible for breaking a sentence apart into tokens. One token at a time. Can be thought of as a lexical
-     * analyzer, scanner or tokenizer
+     * Responsible for breaking a sentence apart into tokens. This is also referred as lexical analysis/tokenization.
      *
      * @return Token with extracted token it values
      */
     private Token getNextToken() {
 
         // If we reach end of file, there are no more token to consume
-        if (textIndex > text.length - 1) {
+        if (tokenizerIndex > text.length - 1) {
             return new Token(EOF, null);
         }
 
-        char currentChar = text[textIndex];
+        char currentChar = text[tokenizerIndex];
 
         if (Character.isDigit(currentChar)) {
-            textIndex++;
+            tokenizerIndex++;
             return new Token(INTEGER, Character.getNumericValue(currentChar));
         } else if (currentChar == '+') {
-            textIndex++;
+            tokenizerIndex++;
             return new Token(PLUS, currentChar);
         } else {
             throw new IllegalArgumentException("Token not supported");
